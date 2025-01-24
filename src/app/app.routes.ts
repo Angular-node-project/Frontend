28c741989bc2d56bdbf2204503f5c55f96eb7805
Sara Salem <sarasalem9898@outlook.com>
2025-01-24 20:06:21 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { SellerComponent } from './seller/seller.component';
+import { ProfileComponent as sellerProfile } from './seller/profile/profile.component';
+import { ProfileComponent as customerProfile }  from './customer/profile/profile.component';
+import { CustomerComponent } from './customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'seller',component:SellerComponent, children:[
+        {path:"profile",component:sellerProfile}
+    ]},
+    {path:'',component:CustomerComponent,children:[
+        {path:"profile",component:customerProfile}
+    ]}
+];

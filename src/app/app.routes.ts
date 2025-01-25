43eb44f012ed3fe1b5af585a317d2fa6e3940bea
Sara Salem <sarasalem9898@outlook.com>
2025-01-25 20:06:28 +0200

    add header footer home to customer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SellerComponent } from './seller/seller.component';
 import { ProfileComponent as sellerProfile } from './seller/profile/profile.component';
 import { ProfileComponent as customerProfile }  from './customer/profile/profile.component';
 import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent as CustomerHome } from './customer/home/home.component';
 
 export const routes: Routes = [
 
@@ -10,6 +11,7 @@ export const routes: Routes = [
         {path:"profile",component:sellerProfile}
     ]},
     {path:'',component:CustomerComponent,children:[
-        {path:"profile",component:customerProfile}
+        {path:"home",component:CustomerHome},
+        {path:"profile",component:customerProfile},
     ]}
 ];
---
+++ src/app/customer/_core/header/header.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-customer-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit {
+  isCollapsed = true;
+  isTransparent: boolean = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      console.log(event.urlAfterRedirects);
+      if (event.urlAfterRedirects == '/home') {
+        this.isTransparent = true;
+        console.log("hhh");
+      } else {
+        this.isTransparent = false;
+      }
+    });
+    console.log(this.isTransparent)
+    // Check if the page is directly accessed
+    if (this.router.url === '/home') {
+      this.isTransparent = true;
+      console.log("Directly on home page, navbar is transparent");
+    } else {
+      this.isTransparent = false;
+      console.log("Directly on another page, navbar is solid");
+    }
+  }
+}

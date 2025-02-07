18a96085214e607876218f847933f0ef2fa1f0a9
Ali <Ali@DESKTOP-HT66DTE>
2025-02-07 14:17:31 +0200

    Checkout Validation

--- src/app/customer/checkout/checkout.component.ts
+++ src/app/customer/checkout/checkout.component.ts
@@ -5,11 +5,13 @@ import { Cart } from 'src/app/_models/cart';
 import { ToastrService } from 'ngx-toastr';
 import { Toast } from 'bootstrap';
 import { Router, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-checkout',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule,ReactiveFormsModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
 })
@@ -19,6 +21,15 @@ export class CheckoutComponent {
   city: string = '';
   zipCode: string = '';
   PhoneNumber: string = '';
+
+  form: FormGroup = new FormGroup({
+    Address: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    zipCode: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    phone_number: new FormControl('',[Validators.required,Validators.pattern(/^(011|012|010|015)\d{8}$/)]),
+  });
+
+
 
   constructor(public cartSer: CartService, private auth: AuthService,private toastr: ToastrService,private route:Router) { }
   ngOnInit(): void {
@@ -34,18 +45,23 @@ export class CheckoutComponent {
     console.log(this.auth.getLoggedInId('customer'));
   }
 
-  addOrder(address: any, zipcode: any, phone_number: any, governorate: any) {
+
+
+
+  addOrder(address: any, zipcode: any, phone_number: any, governorate: any,addInfo:any) {
     let toast=this.toastr
     let route=this.route
     let cart = this.data;
     let product=this.data?.product
     let customer_id=this.data?.customer_id;
+    let additional_data=addInfo||" "
+
     if(!(address && zipcode && phone_number && governorate) ){
       this.toastr.error("Please Complete your Billing details")
     }else{
       console.log("Entered")
       this.cartSer
-      .addOrder({ address, zipcode, phone_number, governorate, product,customer_id })
+      .addOrder({ address, zipcode, phone_number, governorate, product,customer_id,additional_data })
       .subscribe({
         next(e) {
         toast.success(e.message)

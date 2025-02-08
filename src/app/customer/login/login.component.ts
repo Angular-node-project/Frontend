f01a9ad3260fd07bc23d105180021354fbcc5277
Sara Salem <sarasalem9898@outlook.com>
2025-02-08 19:03:01 +0200

    add clerk-role

--- src/app/customer/login/login.component.ts
+++ src/app/customer/login/login.component.ts
@@ -6,6 +6,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
 import { CartService } from '../_services/cart.service';
+import { GeneralService } from '../_services/general.service';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +20,7 @@ export class LoginComponent {
     , private authService: AuthCustomerService
     , private authServiceGeneral: AuthServiceGeneral
     , private router: Router
-    ,private cartService:CartService
+    ,private generalService:GeneralService
   ) { }
   form: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -33,22 +34,11 @@ export class LoginComponent {
         this.authServiceGeneral.saveToken(response.data);
         this.toastr.success('Login successful');
         const returnUrl=this.route.snapshot.queryParams['returnUrl']||'/';
-        if(returnUrl=='/checkout'){
-          var cartData=  localStorage.getItem('cart');
-          var products=cartData? JSON.parse(cartData):[];
-          localStorage.removeItem('cart');
-          if(products?.length>0){
-            this.cartService.addListProductCartGuest(products).subscribe({
-             next:(response)=>{
-              this.cartService.updateCartRegisterdCustomerProductNum();
-              this.router.navigate(['/checkout']);
-             }
-            })
-          }else{
-            this.toastr.error("no products to order");
-          }
+        if(returnUrl=='/checkout'){ 
+        this.generalService.updateCustomerCartAfterAuthentication('/checkout');
         }else{
           localStorage.removeItem('cart');
+          localStorage.removeItem('processedCart');
           this.router.navigate(['/']);
         }
       },

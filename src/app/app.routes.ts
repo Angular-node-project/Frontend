d4d82bcd287708c25fe39a66c49324605a598dd6
Aseel-Elsawy <aseelelsawy7@gmail.com>
2025-02-08 23:36:47 +0200

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,6 +26,8 @@ import { authCustomerGuard } from './customer/authCustomer.guard';
 import { SellersComponent } from './admin/sellers/sellers.component';
 import { CashierComponent } from './admin/cashier/cashier.component';
 import { authAdminGuard } from './admin/authAdmin.guard';
+import { UpdateRequestsComponent } from './admin/update-requests/update-requests.component';
+import { RoleComponent } from './admin/role/role.component';
 
 
 export const routes: Routes = [
@@ -72,7 +74,12 @@ export const routes: Routes = [
                     { path: 'clerks/:page', component: ClerkComponent },
                     {path:'clerks',redirectTo:"clerks/1",pathMatch:'full'},
                     { path: 'seller', redirectTo: '/seller/1', pathMatch: 'full' },
-                    { path: 'seller/:page', component: SellersComponent }
+                    { path: 'seller/:page', component: SellersComponent },
+                    { path: 'UpdateRequests', redirectTo: '/UpdateRequests/1', pathMatch: 'full' },
+                    { path: 'UpdateRequests/:page', component: UpdateRequestsComponent },
+                    { path: 'roles/:page', component: RoleComponent },
+                    {path:'roles',redirectTo:"roles/1",pathMatch:'full'},
+
                 ]
             }
         ]

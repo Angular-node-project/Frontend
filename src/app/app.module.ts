c7e12c199c51c0e2cf87b2a2ecb84b971f23aaac
Ali <Ali@DESKTOP-HT66DTE>
2025-02-15 18:40:55 +0200

    Charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { AddUpdateComponent } from './admin/products/add-update/add-update.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   declarations: [
-    
+
   ],
   imports: [
     BrowserModule,
-    NgbModule,
+    // NgbModule,
     CommonModule,
     AddUpdateComponent,
     NgApexchartsModule
@@ -19,4 +19,4 @@ import { NgApexchartsModule } from 'ng-apexcharts';
   providers: [],
   bootstrap: []
 })
-export class AppModule { } 
+export class AppModule { }
